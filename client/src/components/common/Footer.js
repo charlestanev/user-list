1b8a75da0613ac5e92acd8404f2c98cd9d420ed5
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2022-07-16 18:29:19 +0300

    init
    init

---
+++ client/src/components/common/Footer.js
@@ -0,0 +1,8 @@
+export const Footer = () => {
+	return (
+		// <!-- Footer component  -->
+		<footer class="footer">
+			<p>Copyright © designed by Mihail Valkov</p>
+		</footer>
+	);
+}
---
+++ client/src/components/common/Header.js
@@ -0,0 +1,11 @@
+export const Header = () => {
+	return (
+		// < !--Header component-- > 
+		<header class="header">
+			<div class="logo">
+				<span class="course">React Course - June 2022</span>
+				<span class="description">User List Demo</span>
+			</div>
+		</header>
+	)
+}
